e1e95b5bf3233b47d4ab4266197963aa4b8c9ba7
Abdullah Almsaeed <almasaeed2010@gmail.com>
2016-11-21 12:47:00 -0500

    add tests

--- theme/templates/tripal_feature_properties_no_ssr.tpl.php
+++ theme/templates/tripal_feature_properties_no_ssr.tpl.php
@@ -1,4 +1,4 @@
-<div style="position: fixed; top: 0; left: 0; width: 100px; height: auto;">This is a test</div>
+<div style="position: fixed; top: 0; left: 0; width: 100px; height: auto; z-index: 99999999; background: #fff;">This is a test</div>
 <?php
 $feature = $variables['node']->feature;
 $options = array('return_array' => 1);
