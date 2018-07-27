98a8084f158c95a9fc21fd0cd25ecb6d0c8254d0
Bradford Condon <bradford.condon@gmail.com>
2018-07-27 10:04:02 -0400

    push to confirm transactions asrent wokring

--- tests/DatabaseSeeders/DevSeedSeeder.php
+++ tests/DatabaseSeeders/DevSeedSeeder.php
@@ -13,19 +13,22 @@ class DevSeedSeeder extends Seeder {
    */
 
 //protected $protein_file = ['file_local' => '/path/to/local/file'];
+  protected $landmark_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/master/Fexcel_mini/sequences/empty_landmarks.fasta'];
 
-  protected $mRNA_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/blob/master/Fexcel_mini/sequences/mrna_mini.fasta'];
+  protected $landmark_type = 'scaffold';
 
-  protected $protein_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/blob/master/Fexcel_mini/sequences/polypeptide_mini.fasta'];
+  protected $mRNA_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/master/Fexcel_mini/sequences/mrna_mini.fasta'];
 
-  protected $gff_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/blob/master/Fexcel_mini/gff/filtered.gff'];
+  protected $protein_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/master/Fexcel_mini/sequences/polypeptide_mini.fasta'];
 
-  protected $blast_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/blob/master/Fexcel_mini/gff/filtered.gff'];
+  protected $gff_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/master/Fexcel_mini/gff/filtered.gff'];
+
+  protected $blast_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/master/Fexcel_mini/gff/filtered.gff'];
 
 
-  protected $biomaterial_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/blob/master/Fexcel_mini/biomaterials/biomaterials.xml'];
+  protected $biomaterial_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/master/Fexcel_mini/biomaterials/biomaterials.xml'];
 
-  protected $expression_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/blob/master/Fexcel_mini/expression/expression.tsv'];
+  protected $expression_file = ['file_remote' => 'https://raw.githubusercontent.com/statonlab/tripal_dev_seed/master/Fexcel_mini/expression/expression.tsv'];
 
 //coming soon
   protected $interpro_file = ['file_remote' => NULL];
@@ -36,6 +39,8 @@ class DevSeedSeeder extends Seeder {
 
 protected  $prot_regexp = NULL;
 
+protected $organism_name = 'F. excelsior miniature';
+
 //Don't change these variables here!  Instead, change the values in the factory within the constructor below.
   protected $organism = NULL;
 
@@ -43,23 +48,20 @@ protected  $prot_regexp = NULL;
 
   protected $blastdb = NULL;
 
-  public function constructor() {
+  public function __construct() {
 
     $organism = factory('chado.organism')->create([
       'common_name' => $this->organism_name,
       'genus' => 'Fraxinus',
       'species' => 'excelsior',
       'abbreviation' => 'F. excelsor',
-      'comment' => 'The Tripal Dev Seed miniature dataset.',
+      'comment' => 'Loaded with TripalDev Seed.',
     ]);
 
-    $this->organism_id = $organism;
+    $this->organism = $organism;
     $sequence_analysis = factory('chado.analysis')->create([
-      'common_name' => $this->organism_name,
-      'genus' => 'Fraxinus',
-      'species' => 'excelsior',
-      'abbreviation' => 'F. excelsor',
-      'comment' => 'The Tripal Dev Seed miniature dataset.',
+      'name' => 'Fraxinus exclesior miniature dataset',
+      'description' => 'Tripal Dev Seed',
     ]);
 
     $this->sequence_analysis = $sequence_analysis;
@@ -71,6 +73,28 @@ protected  $prot_regexp = NULL;
    * Uncomment loaders you would like to run.
    */
   public function up() {
+
+    //first load landmarks.
+
+
+    $run_args = [
+      'organism_id' => $this->organism->organism_id,
+      'analysis_id' => $this->sequence_analysis->analysis_id,
+      'seqtype' => $this->landmark_type,
+      'method' => 2, //default insert and update
+      'match_type' => 1, //unique name default
+      //optional
+      're_name' => NULL,
+      're_uname' => NULL,
+      're_accession' => NULL,
+      'db_id' => NULL,
+      'rel_type' => NULL,
+      're_subject' => NULL,
+      'parent_type' => NULL,
+
+    ];
+  //  $this->load_landmarks($run_args, $this->landmark_file);
+
 
     $run_args = [
       'analysis_id' => $this->sequence_analysis->analysis_id,
@@ -94,9 +118,7 @@ protected  $prot_regexp = NULL;
 
     ];
 
-    var_dump($run_args);
-
-    //$this->load_GFF($run_args, $gff_file);
+  //  $this->load_GFF($run_args, $this->gff_file);
 
 
     $run_args = [
@@ -116,7 +138,7 @@ protected  $prot_regexp = NULL;
 
     ];
 
-    //$this->load_mRNA_FASTA($run_args, $mrna_file);
+  //  $this->load_mRNA_FASTA($run_args, $this->mRNA_file);
 
     $run_args = [
       'organism_id' => $this->organism->organism_id,
@@ -142,7 +164,7 @@ protected  $prot_regexp = NULL;
     }
 
 
-    //$this->load_polypeptide_FASTA($run_args, $protein_file);
+   // $this->load_polypeptide_FASTA($run_args, $this->protein_file);
 
     $run_args = [
       'analysis_id' => $this->sequence_analysis->analysis_id,
@@ -153,7 +175,7 @@ protected  $prot_regexp = NULL;
       'parsego' => TRUE,
     ];
 
-    //$this->load_interpro_annotations($run_args, $interpro_file);
+    //$this->load_interpro_annotations($run_args, $this->interpro_file);
 
     $run_args = [
       'analysis_id' => $this->sequence_analysis->analysis_id,
@@ -167,7 +189,7 @@ protected  $prot_regexp = NULL;
       'query_uniquename' => 0,
     ];
 
-    //$this->load_blast_annotations($run_args, $blast_annotation_file);
+   // $this->load_blast_annotations($run_args, $this->blast_file);
 
     $run_args = [
       'organism_id' => $this->organism->organism_id,
@@ -177,7 +199,7 @@ protected  $prot_regexp = NULL;
 //'cvterm_configuration' => NULL,
 //'cvalue_configuration' => NULL];
 
-//	$this->load_biomaterials($run_args, $biomaterial_file);
+	$this->load_biomaterials($run_args, $this->biomaterial_file);
 
     $run_args = [
       'filetype' => 'mat', //matrix file type
@@ -190,65 +212,74 @@ protected  $prot_regexp = NULL;
       'feature_uniquenames' => NULL,
       'quantificationunits' => NULL,
     ];
-    // $this->load_expression($run_args, $expression_file);
+
+     $this->load_expression($run_args, $this->expression_file);
+  }
+
+  private function load_landmarks($run_args, $file) {
+    module_load_include('inc', 'tripal_chado', 'includes/TripalImporter/FASTAImporter');
+
+    $importer = new \FASTAImporter();
+    $importer->create($run_args, $file);
+    $importer->prepareFiles();
+    $importer->run();
   }
 
   private function load_mRNA_FASTA($run_args, $file) {
     module_load_include('inc', 'tripal_chado', 'includes/TripalImporter/FASTAImporter');
-
-    $importer = new FASTAImporter();
-
+    $importer = new \FASTAImporter();
     $importer->create($run_args, $file);
+    $importer->prepareFiles();
     $importer->run();
   }
 
   private function load_polypeptide_FASTA($run_args, $file) {
     module_load_include('inc', 'tripal_chado', 'includes/TripalImporter/FASTAImporter');
 
-    $importer = new FASTAImporter();
-
+    $importer = new \FASTAImporter();
     $importer->create($run_args, $file);
+    $importer->prepareFiles();
     $importer->run();
   }
 
   private function load_interpro_annotations($run_args, $file) {
     module_load_include('inc', 'tripal_analysis_interpro', 'includes/TripalImporter/InterProImporter');
 
-    $importer = new InterProImporter();
-
+    $importer = new \InterProImporter();
     $importer->create($run_args, $file);
+    $importer->prepareFiles();
     $importer->run();
   }
 
   private function load_GFF($run_args, $file) {
-    module_load_include('inc', 'tripal_chado', 'includes/TripalImporter/GFFImporter');
-    $importer = new GFFImporter();
-
+    module_load_include('inc', 'tripal_chado', 'includes/TripalImporter/GFF3Importer');
+    $importer = new \GFF3Importer();
     $importer->create($run_args, $file);
+    $importer->prepareFiles();
     $importer->run();
   }
 
   private function load_blast_annotations($run_args, $file) {
     module_load_include('inc', 'tripal_analysis_blast', 'includes/TripalImporter/BlastImporter');
-    $importer = new BlastImporter();
-
+    $importer = new \BlastImporter();
     $importer->create($run_args, $file);
+    $importer->prepareFiles();
     $importer->run();
   }
 
   private function load_biomaterials($run_args, $file) {
     module_load_include('inc', 'tripal_biomaterial', 'includes/TripalImporter/tripal_biomaterial_loader_v3');
-    $importer = new tripal_biomaterial_loader_v3();
-
+    $importer = new \tripal_biomaterial_loader_v3();
     $importer->create($run_args, $file);
+    $importer->prepareFiles();
     $importer->run();
   }
 
   private function load_expression($run_args, $file) {
     module_load_include('inc', 'tripal_analysis_expression', 'includes/TripalImporter/tripal_expression_data_loader');
-    $importer = new tripal_expression_data_loader();
-
+    $importer = new \tripal_expression_data_loader();
     $importer->create($run_args, $file);
+    $importer->prepareFiles();
     $importer->run();
   }
 }
--- tests/SeederTest.php
+++ tests/SeederTest.php
@@ -3,22 +3,48 @@ namespace Tests;
 
 use StatonLab\TripalTestSuite\DBTransaction;
 use StatonLab\TripalTestSuite\TripalTestCase;
+use test_module\tests\Feature\DatabaseSeederTest;
 
 class SeederTest extends TripalTestCase {
   // Uncomment to auto start and rollback db transactions per test method.
    use DBTransaction;
 
+
+  /**
+   * @group test
+   */
+   public function testBasicsImporter(){
+     $seed = DatabaseSeeders\DevSeedSeeder::seed();
+
+     $query = db_select('chado.organism', 'o');
+     $query->fields('o', ['genus']);
+     $query->condition('o.common_name', 'F. excelsior miniature');
+     $result = $query->execute()->fetchField();
+     $this->assertNotEmpty($result);
+
+
+     $query = db_select('chado.analysis', 'a');
+     $query->fields('a', ['name']);
+     $query->condition('a.name', 'Fraxinus exclesior miniature dataset');
+     $result = $query->execute()->fetchField();
+     $this->assertNotEmpty($result);
+   }
   /**
    * Basic test example.
    * Tests must begin with the word "test".
    * See https://phpunit.readthedocs.io/en/latest/ for more information.
    * @group test
    */
-  public function testBasicExample() {
+  public function testGFFImporter() {
 
     $seed = DatabaseSeeders\DevSeedSeeder::seed();
 
+    $query = db_select('chado.feature', 'f');
+    $query->fields('f', ['name']);
+    $query->join('chado.organism', 'o', 'o.organism_id = f.organism_id');
+    $query->condition('o.common_name', 'F. excelsior miniature');
+    $result = $query->execute()->fetchAll();
+    $this->assertNotEmpty($result);
 
-    $this->assertTrue(true);
   }
 }
